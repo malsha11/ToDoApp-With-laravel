9e4beb2141ef34f1a6e3f22bec412ecbbff6f60c
MalshaDissanayaka11 <admsewwandi@gmail.com>
2023-08-13 22:52:23 +0530

    Create project structure

---
+++ todo-app/routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TodoController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+//home
+Route::get('/', [HomeController::class, "index"])->name('home'); 
+//todo
+Route::get('/todo', [TodoController::class, "index"])->name('todo'); 
